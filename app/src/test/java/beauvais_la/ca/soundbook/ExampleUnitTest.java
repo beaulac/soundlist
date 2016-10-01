9e5b8358b9ebba90ffa70660ea06f0cf3ebc99d5
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2016-10-01 00:14:53 -0400

    Initial commit.

---
+++ app/src/test/java/beauvais_la/ca/soundbook/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package beauvais_la.ca.soundbook;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
